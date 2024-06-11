da9a03f36858a9440ca324d174c9ff4fcc1f9080
aslamiyart <38836093+aslamiyart@users.noreply.github.com>
2024-06-11 12:44:34 +0700

    logout.php

--- logout.php
+++ logout.php
@@ -1,5 +1,7 @@
 <?php
 session_start();
+$cookie_name="login";
+setcookie($cookie_name,"", time() - 3600, "/"); //cookies akan dioverride expire nya menjadi 1 jam kebelakang
 session_destroy();
 header("location:login.php");
 ?>
